c1169316257a20db57adbde6426ed1328259eb34
kacper <kacper.kaiser@gmail.com>
2021-12-21 17:31:01 +0100

    Initial i3 config

---
+++ reschange.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash          
+RESOLUTION=$(xrandr | grep \* | awk '{print $1}')
+
+ case $RESOLUTION in
+
+  3440x1440)
+    xrandr --output HDMI-A-0 --mode 1720x1440 --rate 60
+    ;;
+
+  1720x1440)
+    xrandr --output HDMI-A-0 --mode 3440x1440 --rate 60
+    ;;
+
+esac
